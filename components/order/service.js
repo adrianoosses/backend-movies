673bcf9ef0b25a19069718c3f18a385d8938abf8
aosses <adrianoosses9@gmail.com>
2020-12-05 14:45:41 +0100

    feat: alquilar num dias determinado

--- components/order/service.js
+++ components/order/service.js
@@ -7,11 +7,6 @@ exports.getOrder = async (req, res) =>{
     let orders = await Order.find(query);
     res.json({'orders':orders});
 } 
-/*
-let getOrderByUserId = async (id) =>{
-    let orders = await Order.find({userId: id});
-    return orders;
-}*/
 
 let getOrderByUserId = async (id) =>{
     let orders = await Order.find({userId: id});
@@ -20,35 +15,33 @@ let getOrderByUserId = async (id) =>{
 
 exports.getOrderUser = async(req, res) =>{
     let user = await getUserByEmail(req, res);
-    console.log("getOrderUSer: ", user);
-    //console.log("id: ", user._id)
+    //console.log("getOrderUSer: ", user);
     try{
         order = await getOrderByUserId(user._id);
-        console.log("order: ", order);
+        //console.log("order: ", order);
         if(order.length) res.json({"msg":"1 order was found", "content":order});
         else res.json({"msg":"No order was found", "content":""});
         return true;
     }catch{
         console.log("Error happened");
         order = "";
-        res.status(400).json({"error":"Error"});
+        res.status(400).json({"error":"Error", "content":order});
         return false;
     }
 }
 
-exports.addOrder = async (req, res) =>{
-    let dateRent = new Date();
-    let daysToRent = 7;
-    let dateRefund = new Date(dateRent);
-    dateRefund.setDate(dateRent.getDate() + daysToRent);
-    if((await getOrderByUserId(req.body.userId)).length === 0){
-        let newOrder = new Order({
-            userId: req.body.userId, 
-            movieId: req.body.movieId, 
-            createdAt: dateRent, 
-            refundDate: dateRefund
-        });
+let userHasOrder = async (req, res) =>{
+    return !!(await getOrderByUserId(req.body.userId)).length;
+}
 
+exports.addOrder = async (req, res) =>{
+    if(!(await userHasOrder(req, res))){
+        let {userId, movieId, daysToRent} = req.body;
+        let createdAt = new Date();
+        let refundDate = new Date(createdAt);
+        refundDate.setDate(createdAt.getDate() + daysToRent);
+
+        let newOrder = new Order({userId, movieId, createdAt, refundDate});
         await newOrder.save();
         res.json({"msg":"Added"})
         return true;
